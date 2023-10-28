370b63e8f04cc8b1d2d74a5607bb2dcf28df83d6
Salvador Ferradás <sftpo@pm.me>
2023-10-28 13:52:08 +0200

    chore: moved apple icons to layout using svelte:head

--- front/src/routes/+layout.svelte
+++ front/src/routes/+layout.svelte
@@ -11,6 +11,17 @@
 </script>
 
 <Toast />
+<svelte:head
+	><link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+	<link rel="apple-touch-icon" sizes="48x48" href="/apple-touch-icon-48-48.png" />
+	<link rel="apple-touch-icon" sizes="72x72" href="/apple-touch-icon-72-72.png" />
+	<link rel="apple-touch-icon" sizes="96x96" href="/apple-touch-icon-96-96.png" />
+	<link rel="apple-touch-icon" sizes="144x144" href="/apple-touch-icon-144-144.png" />
+	<link rel="apple-touch-icon" sizes="192x192" href="/apple-touch-icon-192-192.png" />
+	<link rel="apple-touch-icon" sizes="256x256" href="/apple-touch-icon-256-256.png" />
+	<link rel="apple-touch-icon" sizes="384x384" href="/apple-touch-icon-384-384.png" />
+	<link rel="apple-touch-icon" sizes="512x512" href="/apple-touch-icon-512-512.png" /></svelte:head
+>
 <Navigation />
 <Header />
 <main>
